641194cbc74a2eb84dc17bcb14ab5283041a57b3
Andre Tranchezzi <andre@tranchezzi.com.br>
2018-03-31 01:20:11 -0300

    ES6 Update

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-# phone-mask
-Input phone Mask
+# Phone-mask
+
+Project - Input phone Mask
+
+# Used technologies:
+
+* Javascript (ES6)
+* HTML 5 
+* SASS
 
 Demo: http://www.tranchezzi.com.br/phone-mask/
--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
 document.querySelector('#phone').addEventListener('input', function (e) {
-  var x = e.target.value.replace(/\D/g, '').match(/(\d{0,2})(\d{0,4})(\d{0,4})/);
+  let x = e.target.value.replace(/\D/g, '').match(/(\d{0,2})(\d{0,4})(\d{0,4})/);
   e.target.value = !x[2] ? x[1] : '(' + x[1] + ') ' + x[2] + (x[3] ? '-' + x[3] : '');
 });
